4c631e6345684e81ed66f57489915521a3f0fc66
Somya Anand <somyaanand214@gmail.com>
2014-10-06 11:58:40 +0530

    Staging: dgnc: Replace printk with pr_err
    Replaced printk(KERN_ERR) with pr_err()
    
    Signed-off-by: Somya Anand <somyaanand214@gmail.com>

--- drivers/staging/dgnc/dgnc_sysfs.c
+++ drivers/staging/dgnc/dgnc_sysfs.c
@@ -143,7 +143,7 @@ void dgnc_create_driver_sysfiles(struct pci_driver *dgnc_driver)
 	rc |= driver_create_file(driverfs, &driver_attr_pollcounter);
 	rc |= driver_create_file(driverfs, &driver_attr_state);
 	if (rc)
-		printk(KERN_ERR "DGNC: sysfs driver_create_file failed!\n");
+		pr_err("DGNC: sysfs driver_create_file failed!\n");
 }
 
 
@@ -416,7 +416,7 @@ void dgnc_create_ports_sysfiles(struct dgnc_board *bd)
 	rc |= device_create_file(&(bd->pdev->dev), &dev_attr_vpd);
 	rc |= device_create_file(&(bd->pdev->dev), &dev_attr_serial_number);
 	if (rc)
-		printk(KERN_ERR "DGNC: sysfs device_create_file failed!\n");
+		pr_err("DGNC: sysfs device_create_file failed!\n");
 }
 
 
